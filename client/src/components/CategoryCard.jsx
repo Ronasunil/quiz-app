c61ec3751d95349928d07099da67c255f08379e8
Ronasunil <ronasunilcoc@gmail.com>
2025-03-17 02:07:08 +0530

    categories page completed

--- client/src/components/CategoryCard.jsx
+++ client/src/components/CategoryCard.jsx
@@ -1,16 +1,16 @@
-function CategoryCard({ categoryName = "", desc = "" }) {
+function CategoryCard({
+  categoryName = "Unknown",
+
+  img = "https://source.unsplash.com/240x320/?portrait?0",
+}) {
   return (
-    <div className="flex space-x-6">
+    <div className="flex flex-col  h-80 space-x-6">
+      <h4 className="text-xl text-center font-semibold">{categoryName}</h4>
       <img
         alt=""
         className="flex-1 flex-shrink-0 object-cover h-56 mb-4 bg-center rounded-sm dark:bg-gray-500"
-        src="https://source.unsplash.com/240x320/?portrait?0"
+        src={img}
       />
-      <div className="flex flex-col">
-        <h4 className="text-xl font-semibold">{categoryName}</h4>
-        <p className="text-sm dark:text-gray-600">{desc}</p>
-        <div className="flex mt-2 space-x-2"></div>
-      </div>
     </div>
   );
 }
--- client/src/pages/category/category.jsx
+++ client/src/pages/category/category.jsx
@@ -2,17 +2,31 @@ import CategoryCard from "../../components/CategoryCard";
 import Wrapper from "../../components/Wrapper";
 
 function Category() {
+  const categories = [
+    "Docker",
+    "Linux",
+    "Code",
+    "uncategorized",
+    "NodeJs",
+    "React",
+    "Next.js",
+    "SQL",
+    "Django",
+    "DevOps",
+    "bash",
+  ];
   return (
     <Wrapper
       sectionHeading="Explore Quiz Categories"
       sectionDescription="Test your knowledge in various tech domains! Choose a category and start your journey to becoming a tech expert."
     >
-      <CategoryCard />
-      <CategoryCard />
-      <CategoryCard />
-      <CategoryCard />
-      <CategoryCard />
-      <CategoryCard />
+      {categories.map((cat) => (
+        <CategoryCard
+          key={cat}
+          img={`../../../public/assets/${cat}.webp`}
+          categoryName={cat}
+        />
+      ))}
     </Wrapper>
   );
 }
